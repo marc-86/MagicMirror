069c8cbb2616c8cf9064529506431c13f093fa15
marc hueskens <marc.hueskens@aucos.de>
2016-04-27 21:40:28 +0200

    try

--- modules/distance/distance.js
+++ modules/distance/distance.js
@@ -6,24 +6,40 @@ Module.register("Distance",{
         
 	},
 
-	var five = require("johnny-five");
-	var prox = null;
+    var distance = 0;
+	var usonic = require('r-pi-usonic');
+	var sensor = null;
+
+	usonic.init(function (error) {
+		if (error) {
+			Log.info('Error');
+		} else {
+			sensor = usonic.createSensor(14, 15, 450);
+			Log.info('usonic sensor initialised');
+		}
+	});
+	
+	var refreshDistance = function () {
+		setTimeout(function () {
+			this.distance = sensor().toFixed(2);
+			console.log('Distance: ' + this.distance + ' cm');
+		}, 60);
+	}
 	
 	// Override dom generator.
 	getDom: function() {
 		var wrapper = document.createElement("div");
-		wrapper.innerHTML = this.config.text;
+		wrapper.innerHTML = "Distanz: " + this.distance;
 		  return wrapper;
 	}
     
     onStart: function() {
-		Log.info("Starting module: " + this.name);
-
-		
+		Log.info("Starting module: " + this.name);		
 
 		// Schedule update interval.
 		var self = this;
 		setInterval(function() {
+		    self.refreshDistance();
 			self.updateDom();
 		}, 1000);
     }
