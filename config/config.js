84503b391e6c61a886c1f71bffa918d9defc64a6
marc-86 <marc.hueskens@googlemail.com>
2016-07-06 21:29:09 +0200

    Config geaendert

--- config/config.js
+++ config/config.js
@@ -15,7 +15,8 @@ var config = {
 		{
 			module: 'alert',
 		},
-		{	module: 'distance',
+		{
+			module: 'distance',
 		},
 		{
 			module: 'clock',
@@ -51,8 +52,8 @@ var config = {
 			position: 'top_right',
 			header: 'Weather Forecast',
 			config: {
-	            location: 'Eschweiler',
-	            appid: 'cf75c38780f8a019aa5de48d17cd39a9'
+				location: 'Eschweiler',
+				appid: 'cf75c38780f8a019aa5de48d17cd39a9'
 			}
 		},
 		{
@@ -70,21 +71,49 @@ var config = {
 			}
 		},
 		{
-        	module: 'MMM-Wunderlist',
-        	position: 'top_left',
-        	header: 'Einkaufsliste',
-        	config: {
-            	accessToken: '0632cd80a6846448755aa50fdafed4ae62433942e08e1ad971ca4929d59e',
+			module: 'MMM-Wunderlist',
+			position: 'top_left',
+			header: 'Einkaufsliste',
+			config: {
+				accessToken: '0632cd80a6846448755aa50fdafed4ae62433942e08e1ad971ca4929d59e',
 				clientID: '1ac41561ee34529a647a',
 				lists: ['Einkaufsliste Hit', 'Einkaufsliste Aldi'],
-        	},
+			},
 		},
-    	{
-        	module: 'MMM-PIR-Sensor',
-        	config: {
-            // See 'Configuration options' for more information.
-        	}
-    	},
+		{
+			module: 'MMM-Traffic',
+			position: 'bottom_right',
+			classes: 'dimmed medium', //optional, default is 'bright medium', only applies to commute info not route_name
+			config: {
+				api_key: 'AIzaSyBVRrfpaE_YLtHe2nYyQqLpbiYmEsi1uXg',
+				mode: 'driving',
+				origin: 'Martin-Luther-Strasse 28, 52249 Eschweiler',
+				destination: 'Hansemannplatz, Aachen',
+				route_name: 'Home to Work',
+				changeColor: true,
+				showGreen: false,
+				limitYellow: 10,
+				limitRed: 20,
+				traffic_model: 'pessimistic',
+				interval: 120000 //2 minutes
+			}
+		},
+		{
+			module: 'MMM-PIR-Sensor',
+			config: {
+			}
+		},
+		{
+			module: 'MMM-Podcast',
+			config: {
+				// See 'Configuration options' for more information.
+			}
+		},
+		{
+			module: 'PIR-Sensor',
+			config: {
+			}
+		},
 		// {
         // 	module: 'distance',
         // 	position: 'top_center',
@@ -94,4 +123,4 @@ var config = {
 };
 
 /*************** DO NOT EDIT THE LINE BELOW ***************/
-if (typeof module !== 'undefined') {module.exports = config;}
+if (typeof module !== 'undefined') { module.exports = config; }
